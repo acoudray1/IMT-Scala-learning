492cccd4f125dc687cbdf81746dd35a0c2384a48
axel COUDRAY <axel.hpm.coudray@gmail.com>
2020-04-03 17:43:12 +0200

    tp1.2 part 4 end

--- tp1/src/money/Account.scala
+++ tp1/src/money/Account.scala
@@ -5,13 +5,18 @@ import money.Currency.Currency
 class Account(var argent: Double, var currency: Currency) {
   require(currency != null)
 
+
   def this(n: Double) {
     this(n, Currency.Euro)
   }
 
-  def +(moreMoney: Double, moneyCurrency: Currency) = {
+  // Adds money on current instance
+  def addMoneyOnAccount(moreMoney: Double, moneyCurrency: Currency) = {
     argent += moreMoney * Currency.exchangeRate(currency)(moneyCurrency)
   }
+
+  // Adds Account on new Account
+  def +(that: Account): Account = Account(argent + (Currency.exchangeRate(that.currency)(currency) * that.argent), currency)
 
   override def toString: String = argent + " " + currency.toString
 }
@@ -19,11 +24,14 @@ class Account(var argent: Double, var currency: Currency) {
 object Account {
 
   var a1 = Account(10, Currency.Euro)
+  var a2 = Account(12, Currency.Yen) + a1
 
   def main(args: Array[String]): Unit = {
-    println(a1.toString)
-    a1 + (12, Currency.Dollar)
-    println(a1.toString)
+    println("a1 account: " + a1.toString)
+    println("a2 account with a1 account added on a2: " + a2.toString)
+
+    a1.addMoneyOnAccount(12, Currency.Dollar)
+    println("a1 account after adding money: " + a1.toString)
   }
 
   def apply(argent: Double, currency: Currency): Account = new Account(argent, currency)
