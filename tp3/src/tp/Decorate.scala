eae1b16f4abd91e12756f674b0adb08f2e8ae919
axel COUDRAY <axel.hpm.coudray@gmail.com>
2020-04-24 15:11:16 +0200

    tp3 part 2

--- tp3/src/tp/Decorate.scala
+++ tp3/src/tp/Decorate.scala
@@ -1,16 +1,16 @@
 package tp
 
-object Test {
+object Decorate {
   def decorate(left: String, s: String, right: String):String = left + s + right
 
   val decorateQ1 = (mot: String) => decorate("\"", mot, "\"") // typage à gauche
 
-  val decorateQ2 = (mot => decorate("\"", mot, "\"")): String => String // typage à droite
+  val decorateQ2: String => String = (mot => decorate("\"", mot, "\"")) // typage à droite
 
   val decorateQ3 = decorate("\"", _, "\"") // typage partiel
 
   def main(args: Array[String]): Unit = {
-    // Partie 1
+    // Partie 2 : Méthodes et fonctions
     println(decorate("Hello", " ", "World!"))
     println(decorateQ1("Hello World!"))
     println(decorateQ2("Hello World!"))
