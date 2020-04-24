54bc451b591e2ce75562522e46e7ddbe7d792e4c
axel COUDRAY <axel.hpm.coudray@gmail.com>
2020-04-24 15:38:16 +0200

    tp3 part 3 done

--- tp3/src/tp/Decorate.scala
+++ tp3/src/tp/Decorate.scala
@@ -1,6 +1,9 @@
 package tp
 
 object Decorate {
+  implicit val decoration: String = "\""
+  implicit val decoration2: (String, String) = ("\"", "\"")
+
   def decorate(left: String, s: String, right: String):String = left + s + right
 
   val decorateQ1 = (mot: String) => decorate("\"", mot, "\"") // typage à gauche
@@ -9,11 +12,22 @@ object Decorate {
 
   val decorateQ3 = decorate("\"", _, "\"") // typage partiel
 
+  def decorateC(mot: String)(implicit s: String) = decorate(s, mot, s)
+  def decorateC2(s: String)(implicit d :(String, String)) = decorate(d._1, s, d._2)
+
   def main(args: Array[String]): Unit = {
     // Partie 2 : Méthodes et fonctions
+    println("*** Partie 2 ***")
     println(decorate("Hello", " ", "World!"))
     println(decorateQ1("Hello World!"))
     println(decorateQ2("Hello World!"))
     println(decorateQ3("Hello World!"))
+
+    // Partie 3 : Curry
+    println("\n*** Partie 3 ***")
+    println(decorateC("Hello World!")("\""))
+    println(decorateC("Hello World!"))
+    println(decorateC2("Hello World!")("\'", "\'"))
+    println(decorateC2("Hello World!"))
   }
 }
